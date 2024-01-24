6d1cd8bb171d2a3f58fea5e42c273323bb94beda
megala12mr@gmail.com <megala12mr@gmail.com>
2024-01-24 19:52:07 +0530

    Remove file

--- coinFlip.js
+++
@@ -1,8 +0,0 @@
-function flipCoin() {
-    const random = Math.floor(Math.random() * 2);
-    return (random === 0) ? "Heads" : "Tails";
-  }
-  
-  const result = flipCoin();
-  
-  console.log(`The coin landed on: ${result}`);
